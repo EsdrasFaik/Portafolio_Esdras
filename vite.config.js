afbb6c2f2726b9ea5be5fd5bf6ea145494eca688
Esdras <rankeygd@gmail.com>
2024-07-06 15:35:29 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import path, { resolve } from "node:path";
 export default defineConfig(
     {
         appType: "mpa",
-        base: "/pw2_vitempa-main/",
+        base: "/PORTAFOLIO/",
         build: {
             rollupOptions: {
                 input: Object.fromEntries(
